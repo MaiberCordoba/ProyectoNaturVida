bf3dda1cd8b49a2a42756a8bfada55c9097035bb
MaiberCordoba <cordobamaiber@gmail.com>
2025-03-09 18:27:17 -0500

    creando controlador que verifica que se escuentre un token en la solicitud

--- src/token/tokenController.MJC.js
+++ src/token/tokenController.MJC.js
@@ -1,5 +1,7 @@
 import { prisma } from "../db.MJC.js";
 import jwt from "jsonwebtoken";
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const tokenVendedoresMJC = async (req, res) => {
     try {
@@ -30,3 +32,40 @@ export const tokenVendedoresMJC = async (req, res) => {
         res.status(500).json({ message: "Error al autenticar usuario" });
     }
 };
+
+const verifyJWT = async(req,res,next) => {
+    try{
+        const token = req.headers['authorization']?.split(" ")[1];
+        if(!token){
+            return res.status(401).json({msg:"Token not provided"})
+        }
+        const verified = jwt.verify(token,process.env.AUTH_SECRET);
+        if(!verified){
+            return res.status(401).json({msg:"Invalid token"});
+        };
+        req.user = verified;
+        next();
+    }
+    catch(error){
+        switch(error.message){
+            case 'invalid signature': {
+                return res.status(401).json({msg:"Invalid token"});
+            }
+            case 'jwt expired': {
+                return res.status(401).json({msg:"Token expired"});
+            }
+            case 'jwt malformed': {
+                return res.status(401).json({msg:"Token malformed"});
+            }
+            case 'invalid token': {
+                return res.status(401).json({msg:"Invalid token"});
+            }
+            default: {
+                res.status(500).json({msg:"Error authenticating"});
+                console.error(error);
+            }
+        }
+    }
+};
+
+export default verifyJWT;
