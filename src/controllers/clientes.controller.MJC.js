44d0d6aff67c9c0560396124bd317f2ca5a05556
MaiberCordoba <cordobamaiber@gmail.com>
2025-03-09 16:02:24 -0500

    creando las solicitudes get por id, delete, put

--- src/controllers/clientes.controller.MJC.js
+++ src/controllers/clientes.controller.MJC.js
@@ -30,3 +30,60 @@ export const CrearClientesMJC  = async(req,res) =>{
     }
 }
 
+export const editarClientesMJC = async (req,res)=>{
+    try {
+        const editarCliente = await prisma.clientesMJC.update({
+            where:{
+                cl_documento: parseInt(req.params.cl_documento)
+            },
+            data: req.body,
+        });
+        if (editarCliente) {
+            return res.status(200).json(editarCliente)
+        }else{
+            return res.status(400).json({message:"no fue posible editar info del cliente"})
+        }
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({message:"sistema"})
+    }
+}
+
+export const eliminarClienteMJC = async (req,res)=>{
+    try {
+        const borrado = await prisma.clientesMJC.delete({
+            where:{
+                cl_documento: parseInt(req.params.cl_documento)
+            }
+        })
+        if (borrado) {
+            return res.status(200).json({ message: "Cliente eliminado con éxito" });
+        }
+        else{
+            return res.status(404).json({ message: "no fue posible eliminar con exito" });
+        }
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({message:"sistema"})
+    }
+}
+
+export const buscarClientesMJC = async (req,res)=>{
+    try {
+        const buscarCliente = await prisma.clientesMJC.findFirst({
+            where:{
+                cl_documento: parseInt(req.params.cl_documento)
+            },
+        });
+        if (buscarCliente) {
+            return res.status(200).json(buscarCliente)
+        }else{
+            return res.status(400).json({message:"no fue posible encontrar info del cliente"})
+        }
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({message:"sistema"})
+    }
+}
+
+
--- src/routes/clientes.router.MJC.js
+++ src/routes/clientes.router.MJC.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { CrearClientesMJC, listarClientesMJC } from "../controllers/clientes.controller.MJC.js";
+import { buscarClientesMJC, CrearClientesMJC, editarClientesMJC, eliminarClienteMJC, listarClientesMJC } from "../controllers/clientes.controller.MJC.js";
 
 const routeClientMJC = Router();
 
 routeClientMJC.get("/client",listarClientesMJC)
 routeClientMJC.post("/client",CrearClientesMJC)
+routeClientMJC.put("/client/:cl_documento",editarClientesMJC)
+routeClientMJC.delete("/client/:cl_documento",eliminarClienteMJC)
+routeClientMJC.get("/client/:cl_documento",buscarClientesMJC)
+
 
 export default routeClientMJC;
