8bc6c66acd502bba7f914d60a00dc09bf0686688
MaiberCordoba <cordobamaiber@gmail.com>
2025-03-09 17:52:05 -0500

    creando controlador para crear token

---
+++ src/token/tokenController.MJC.js
@@ -0,0 +1,33 @@
+import { prisma } from "../db.MJC.js";
+import jwt from "jsonwebtoken";
+
+export const tokenVendedoresMJC = async (req, res) => {
+    try {
+        const { identificacion, password } = req.body;
+
+        // 📌 Buscar usuario en la base de datos con Prisma
+        const vendedor = await prisma.vendedoresMJC.findFirst({
+            where: {
+                venUsuario: identificacion,
+                venContraseña: password, 
+            },
+          
+        });
+
+        if (vendedor) {
+            // 📌 Generar token JWT
+            const token = jwt.sign(
+                { user: vendedor },
+                process.env.AUTH_SECRET,
+                { expiresIn: process.env.AUTH_EXPIRES }
+            );
+
+            return res.status(200).json({ message: "Usuario autenticado", token });
+        } else {
+            return res.status(404).json({ message: "Usuario no autenticado" });
+        }
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ message: "Error al autenticar usuario" });
+    }
+};
