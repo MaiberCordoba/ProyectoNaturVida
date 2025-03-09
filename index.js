cc5e2e1b13e5a8496d9d22aeb17adfd4424c8d91
MaiberCordoba <cordobamaiber@gmail.com>
2025-03-09 14:40:56 -0500

    montando las rutas de clientes en el enrutador principal

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+//importaciones
 import express from "express";
+import routeClientMJC from "./src/routes/clientes.router.MJC.js";
 
+//inicializaciones
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
+//ruta clientes
+app.use("/api", routeClientMJC);
+
+
+//servidor
 app.listen(3000,()=>{
     console.log("3000")
 })
