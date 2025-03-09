3feb01a2e5b8665056e91cdcf00f00cea4b77d15
MaiberCordoba <cordobamaiber@gmail.com>
2025-03-09 18:48:56 -0500

    montando rutas de Vendedores

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 import express from "express";
 import routeClientMJC from "./src/routes/clientes.router.MJC.js";
 import routerToken from "./src/token/tokenRouter.MJC.js";
+import routeVendedoresMJC from "./src/routes/vendedores.router.MJC.js";
 
 //inicializaciones
 const app = express();
@@ -10,7 +11,13 @@ app.use(express.urlencoded({extended:false}));
 
 //ruta clientes
 app.use("/api", routeClientMJC);
+
+//token
 app.use("/api",routerToken)
+
+//vendedores
+app.use("/api",routeVendedoresMJC)
+
 
 //servidor
 app.listen(3000,()=>{
